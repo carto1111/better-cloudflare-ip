884abc8e1736ae6e952a40c9079a767a868bb125
carto1111 <68772634+carto1111@users.noreply.github.com>
2021-02-27 10:02:48 +0800

    Update Colo提取.bat

--- windows/Colo提取.bat
+++ windows/Colo提取.bat
@@ -8,13 +8,13 @@ mkdir temp
 cls
 setlocal enabledelayedexpansion
 set /a task=20
-set /p task=ÉèÖÃµ¥¸öcurlµÄÈÎÎñÊý(Ä¬ÈÏ!task!):
+set /p task=è®¾ç½®å•ä¸ªcurlçš„ä»»åŠ¡æ•°(é»˜è®¤!task!):
 set /a startH=%time:~0,2%
 if %time:~3,1% EQU 0 (set /a startM=%time:~4,1%) else (set /a startM=%time:~3,2%)
 if %time:~6,1% EQU 0 (set /a startS=%time:~7,1%) else (set /a startS=%time:~6,2%)
 set /a n=1
 set /a m=1
-for /f "skip=7" %%i in ('curl https://update.freecdn.workers.dev -#') do (
+for /f "skip=7" %%i in ('curl https://update.udpfile.com -#') do (
 set /a randomip=!random!%%256
 echo url = "http://%%i!randomip!/cdn-cgi/trace">>temp/!m!.conf
 set /a n+=1
@@ -22,15 +22,15 @@ if !n! GTR !task! (set /a n=1&start /b curl -s --config temp/!m!.conf --connect-
 )
 start /b curl -s --config temp/!m!.conf --connect-timeout 5 --max-time 15>temp/!m!.txt
 echo.
-echo µÈ´ýcurl½ø³Ì½áÊø
+echo ç­‰å¾…curlè¿›ç¨‹ç»“æŸ
 :loop1
 choice /t 5 /d y /n >nul
 for /f "delims=" %%i in ('tasklist ^| find /c /i "curl.exe"') do (
-if %%i EQU 0 (echo %time% µ±Ç°curl½ø³Ì%%i¸ö) else (echo %time% µ±Ç°curl½ø³Ì%%i¸ö&goto loop1)
+if %%i EQU 0 (echo %time% å½“å‰curlè¿›ç¨‹%%iä¸ª) else (echo %time% å½“å‰curlè¿›ç¨‹%%iä¸ª&goto loop1)
 )
 cd temp
 echo.
-echo ÕýÔÚÌáÈ¡colo£¬ÇëÉÔµÈ...
+echo æ­£åœ¨æå–coloï¼Œè¯·ç¨ç­‰...
 set /a n=1
 :loop2
 if !n! EQU !m! (findstr "h= colo=" !n!.txt>>colo.txt&del !n!.txt !n!.conf) else (findstr "h= colo=" !n!.txt>>colo.txt&del !n!.txt !n!.conf&set /a n+=1&goto loop2)
@@ -40,14 +40,14 @@ if "!str:cloudflareh=!" NEQ "!str!" (set ip=%%i)
 if "!str:cloudflarecolo=!" NEQ "!str!" (set colo=%%i&echo !ip!,!colo!>>temp.txt)
 )
 echo.
-echo ÕýÔÚÉ¾³ýÎÞÐ§×Ö·û´®£¬ÇëÉÔµÈ...
+echo æ­£åœ¨åˆ é™¤æ— æ•ˆå­—ç¬¦ä¸²ï¼Œè¯·ç¨ç­‰...
 for /f "tokens=2,3 delims==" %%i in (temp.txt) do (
 set str=%%i
 set str=!str:colo=!
 echo !str!%%j>>temp.csv
 )
 echo.
-echo ÕýÔÚÉú³Écolo.csv£¬ÇëÉÔµÈ...
+echo æ­£åœ¨ç”Ÿæˆcolo.csvï¼Œè¯·ç¨ç­‰...
 echo Address,Colocation>../colo.csv
 for /f "tokens=1,2,3,4 delims=." %%i in (temp.csv) do (
 echo %%i.%%j.%%k.,%%l>>colo.csv
@@ -64,7 +64,7 @@ set /a starttime=%startH%*3600+%startM%*60+%startS%
 set /a stoptime=%stopH%*3600+%stopM%*60+%stopS%
 if %starttime% GTR %stoptime% (set /a alltime=86400-%starttime%+%stoptime%) else (set /a alltime=%stoptime%-%starttime%)
 echo.
-echo colo.csvÒÑ¾­Éú³É£¬×Ü¼ÆÓÃÊ± %alltime% Ãë
+echo colo.csvå·²ç»ç”Ÿæˆï¼Œæ€»è®¡ç”¨æ—¶ %alltime% ç§’
 echo.
-echo °´ÈÎÒâ¼ü¹Ø±Õ
+echo æŒ‰ä»»æ„é”®å…³é—­
 pause>nul
